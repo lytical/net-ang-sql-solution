97177e9a4dd64184f178b92ac302aabca2a8c958
Barry Hayles <barry.hayles@trwd.com>
2026-02-19 16:12:25 -0600

    added client environments and site library

---
+++ client/projects/site/src/lib/site.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lyt-site-site',
+  imports: [],
+  template: `
+    <p>
+      site works!
+    </p>
+  `,
+  styles: ``,
+})
+export class Site {
+
+}
---
+++ client/projects/site/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of site
+ */
+
+export * from './lib/site';
